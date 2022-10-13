8036fbff2b453518082fa62c18a63e286fddb2bd
TimotejBalajka <113421940+TimotejBalajka@users.noreply.github.com>
2022-10-13 09:01:01 +0200

    Update Numbers.cs

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._06
 {
@@ -6,7 +6,25 @@ namespace Open_Lab_04._06
     {
         public int[] NoOdds(int[] numbers)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            for (int i =0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            int[] evens = new int[count];
+            int t = 0;
+            for (int x =0; x < numbers.Length; x++)
+            {
+                if (numbers[x] % 2 == 0)
+                {
+                    evens[t] = numbers[x];
+                    t++;
+                }
+            }
+            return evens;
         }
     }
 }
